8b2310f66cc1a85e12cd1b548e4a12a28a41d131
ShaQayeQ <shaghay3gh@gmail.com>
2025-01-21 21:35:28 +0100

    try to handle errors

--- 06-raw-http.js
+++ 06-raw-http.js
@@ -1,7 +1,7 @@
 const http = require("node:http");
 
 const url =
-	"http://api.weatherapi.com/v1/current.json?key=a150a97e684d45b2860115533251201&=51.300,12.33";
+	"http://api.weatherap.com/v1/current.json?key=a150a97e684d45b2860115533251201&q=51.300,12.33";
 
 //! return value for request()
 const request = http.request(url, (response) => {
@@ -15,16 +15,20 @@ const request = http.request(url, (response) => {
 
 		data += chunckOfData.toString();
 		// data += chunckOfData //! is already returning a string!
-		console.log(data);
+		console.log("data", data);
 		// process.stdout.write(data); // same as console.log()
 
 		const body = JSON.parse(data);
-		// console.log(body);
-		const { location, current } = body;
-		console.log(
-			`The weather in ${location.name} is ${current.temp_c} degree C and it feels like ${current.feelslike_c} degree C.
+		console.log("body", body);
+		const { location, current, error } = body;
+		if (error) {
+			console.log("error:", error.message);
+		} else {
+			console.log(
+				`The weather in ${location.name} is ${current.temp_c} degree C and it feels like ${current.feelslike_c} degree C.
 Weather condition is ${current.condition.text}.`
-		);
+			);
+		}
 	});
 	// listens to end event
 	// response.on("end", () => {
@@ -34,7 +38,12 @@ Weather condition is ${current.condition.text}.`
 
 //! listens for errors
 request.on("error", (e) => {
-	console.log("ERROR!");
+	if (typeof e === JSON) {
+		const error = JSON.parse(e);
+		console.log("ERROR!", error);
+	} else {
+		console.log(e);
+	}
 });
 
 request.end(); //! crucial method
